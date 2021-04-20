e524d3d21dd0ee397a59c3e099294a192e9c4f56
snowjet <rarm.jet@gmail.com>
2021-04-20 12:04:24 +1000

    add script to create tekton resources

---
+++ create_tekton.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z $1 ]] && [[ -z $PROJECT ]]; then
+    echo "Pass project into script of set PROJECT env var"
+    exit 1
+fi
+
+if [[ -z $PROJECT ]]; then
+   export PROJECT="${1}"
+fi
+
+oc new-project "${PROJECT}"
+
+cat ./tekton/pipeline-ws/*.yml | envsubst '${PROJECT}' | oc apply -n ${PROJECT} -f -
+
+# There is a bug that requires this to be applied with the oc/kubectl create command
+oc create ../tekton/pipeline-ws/pipelineRun.yml
